04df55008be95bec1f187af8fbf1d066da2b717c
netiapaul <netiapaul23@gmail.com>
2020-05-11 14:23:38 +0300

    app completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
 
 class MyHomePage extends StatefulWidget {
@@ -10,8 +11,91 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('i am headed to greatness'),
+      body: ChannelListView(
+        filter: {
+          'members': {
+            '\$in': [StreamChat.of(context).user.id],
+          }
+        },
+        sort: [SortOption('last_message_at')],
+        pagination: PaginationParams(
+          limit: 20,
+        ),
+        channelWidget: ChannelPage(),
+      )
+    );
+  }
+}
+
+class ChannelPage extends StatelessWidget {
+
+const ChannelPage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+ Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: ChannelHeader(),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Stack(
+              children: <Widget>[
+                MessageListView(
+                  threadBuilder: (_, parentMessage) {
+                    return ThreadPage(
+                      parent: parentMessage,
+                    );
+                  },
+                ),
+                Positioned.fill(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0,
+                      vertical: 4,
+                    ),
+                    child: TypingIndicator(
+                      alignment: Alignment.bottomRight,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          MessageInput(),
+        ],
+      ),
+    );
+  }
+}
+
+class ThreadPage extends StatelessWidget {
+  final Message parent;
+
+  ThreadPage({
+    Key key,
+    this.parent,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: ThreadHeader(
+        parent: parent,
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: MessageListView(
+              parentMessage: parent,
+            ),
+          ),
+          if (parent.type != 'deleted')
+            MessageInput(
+              parentMessage: parent,
+            ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,50 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'package:animated_splash/animated_splash.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  final client = Client(
+    'vcjqqdamyrka',
+    logLevel: Level.INFO,
+  );
+
+  await client.setUser(
+    User(id: '78828'),
+    'x48zsnazdcum7jm4rwp4jdxsdc4f77nabyu47c74bp28hh4nx6ynvm7qunnaeuz3',
+  );
+  runApp(MyApp(client));
 }
 
 Map<int, Widget> op = {1: MyHomePage()};
 
 class MyApp extends StatelessWidget {
+
+      final Client client;
+       MyApp(this.client);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
       home: AnimatedSplash(
         imagePath: 'images/splash.png',
-        home: MyHomePage(),
+        home:Container (
+            child: StreamChat(
+            client: client,
+            child: MyHomePage(),
+          ),
+          
+        ),
         duration: 4500,
         type: AnimatedSplashType.StaticDuration,
         outputAndHome: op,
       ),
       debugShowCheckedModeBanner: false,
+      color: Colors.green,
     );
     
   }
